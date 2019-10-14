dfafefd3d1530f6c4693367fb7268b01cd12b39f
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-14 15:13:44 -0300

    Desafio 01 - Aula 048

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edite o <code>src/App.js</code> e salve para recarregar.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Aprendendo React
-                </a>
-            </header>
-        </div>
+        <a href="#" className="action">
+            Sobre
+        </a>
     );
 }
 
