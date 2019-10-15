2c0a4b2490317ca5d5e5c4318511ddb90bdfc9e9
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-15 15:54:39 -0300

    Desafio 02 - Aula 048

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./generics/normalize.css";
+import "./elements/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
