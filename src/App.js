45b279820fa70e782860731e710fc450db4a102c
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-17 18:27:39 -0300

    Desafio 02 - Aula 050

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Board from "./objects/Board";
+import Marcacao from "./objects/Marcacao";
 
 function App() {
     return (
         <>
             <Header />
             <Board />
+            <Marcacao />
         </>
     );
 }
---
+++ src/objects/Marcacao/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Circle from "../../images/Circle.png";
+import X from "../../images/X.png";
+
+const Marcacao = () => {
+    return (
+        <>
+            <img src={Circle} alt="Circle" />
+            <img src={X} alt="X" />
+        </>
+    );
+};
+
+export default Marcacao;
