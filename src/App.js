113bcc991cf38868a3e3bb141e293ef7921c29aa
João Moraes <joao.moraes@ivoiredesign.com.br>
2019-10-17 17:49:32 -0300

    Desafio 01 - Aula 050

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Board from "./objects/Board";
 
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Board />
+        </>
+    );
 }
 
 export default App;
