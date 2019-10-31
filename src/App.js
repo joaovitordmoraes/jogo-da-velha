9c97f58e78e1ab5c39e7cbe5e38c78e7cb81f517
Joao Moraes <joaovitordmoraes@gmail.com>
2019-10-30 23:16:56 -0300

    Ajustado o posicionamento do board e do header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 
 function App() {
     return (
-        <>
+        <main className="app">
             <Header />
             <Hashtag />
-        </>
+        </main>
     );
 }
 
