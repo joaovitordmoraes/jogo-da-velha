169be87960c38ff57007a4c8e4d04af2e7c84fd8
joaovitordmoraes <joaovitordmoraes@gmail.com>
2019-12-18 14:23:24 -0200

    Criado o Checkbox object e deixei o Input mais simples

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
-import Input from "./objects/Input";
+import Checkbox from "./objects/Checkbox";
 
 const App = () => {
     return (
         <main className="app">
             <Header />
             <Hashtag />
-            <Input id="show" value="show" content="Mostrar eventos" type="checkbox" />
+            <Checkbox id="show" value="show" content="Mostrar eventos" type="checkbox" />
         </main>
     );
 }
---
+++ src/objects/Checkbox/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Input from "../Input";
+import Label from "../Label";
+
+const Checkbox = ({id = "", value = "", content = ""}) => {
+
+    return (
+        <>
+            <Input id={id} type="checkbox" value={value} content={content} />
+            <Label htmlFor={id} content={content} />
+        </>
+    );
+};
+
+export default Checkbox;
