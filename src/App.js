6370e317baef9307691ee5461e6069dd12a3e05a
joaovitordmoraes <joaovitordmoraes@gmail.com>
2019-12-18 12:19:06 -0200

    Criado o object do Input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import Label from "./objects/Label";
+import Input from "./objects/Input";
 
 const App = () => {
     return (
         <main className="app">
             <Header />
             <Hashtag />
+            <Input value="show" />
             <Label content="Mostrar eventos" />
         </main>
     );
