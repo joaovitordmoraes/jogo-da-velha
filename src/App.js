df9156394aa70128c04002c923ab0c1403fd83bf
joaovitordmoraes <joaovitordmoraes@gmail.com>
2019-12-18 12:12:56 -0200

    Criado o object da Label

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
+import Label from "./objects/Label";
 
 const App = () => {
     return (
         <main className="app">
             <Header />
             <Hashtag />
+            <Label content="Mostrar eventos" />
         </main>
     );
 }
