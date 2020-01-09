ac3e663123c891ff768db1b8f2dd7f84a590639c
Joao Moraes <joaovitordmoraes@gmail.com>
2020-01-09 11:28:12 -0200

    Criado o object IconSocial

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./styles.css";
 import Profile from "../../images/profile.jpg";
+import Github from "../../images/github.png";
+import Linkedin from "../../images/linkedin.png";
+import Facebook from "../../images/facebook.png";
+import Twitter from "../../images/twitter.png";
+import Instagram from "../../images/instagram.png";
 
 import AvatarProfile from "../../objects/AvatarProfile";
+import IconSocial from "../../objects/IconSocial";
 
 
 const ProfileUser = () => {
@@ -13,6 +19,13 @@ const ProfileUser = () => {
             </dd>
             <dt className="title">João Moraes</dt>
             <dd className="description">Dev Front-End, de Piracicaba para São Paulo. Nos momentos livres gamer dedicado.</dd>
+            <dd className="socials">
+                <IconSocial src={Github} alt="Logo do Github" />
+                <IconSocial src={Linkedin} alt="Logo do Linkedin" />
+                <IconSocial src={Facebook} alt="Logo do Facebook" />
+                <IconSocial src={Twitter} alt="Logo do Twitter" />
+                <IconSocial src={Instagram} alt="Logo do Instagram" />
+            </dd>
         </dl>
     );
 }
