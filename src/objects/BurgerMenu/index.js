3e3b2d99fd9dd0e5a3839b7bdc2c643971e13014
Joao Moraes <joaovitordmoraes@gmail.com>
2020-01-09 14:58:56 -0200

    Adicionado evento de click no BurgerMenu

--- src/objects/BurgerMenu/index.js
+++ src/objects/BurgerMenu/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles.css";
 
+const handleClick = () => {
+    console.log("clicou!!!")
+}
+
 const BurgerMenu = () => {
     return (
-        <a href="#menu" className="burger-menu">
+        <a href="#menu" className="burger-menu" onClick={handleClick}>
             <span className="center">Menu</span>
         </a>
     );
