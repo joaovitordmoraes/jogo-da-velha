c530cc8f0686a4ab146945ac182668a2ce9bf9ac
Joao Moraes <joaovitordmoraes@gmail.com>
2020-01-10 11:46:59 -0200

    Refactory do object Input para InputField

--- src/objects/CheckboxEvents/index.js
+++ src/objects/CheckboxEvents/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.css";
-import Input from "../Input";
+import InputField from "../InputField";
 import Label from "../Label";
 
 const Checkbox = ({id = "", value = "", content = ""}) => {
 
     return (
         <div className="checkbox-events">
-            <Input id={id} type="checkbox" value={value} content={content} />
+            <InputField id={id} type="checkbox" value={value} content={content} />
             <Label htmlFor={id} content={content} />
         </div>
     );
