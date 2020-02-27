f6f55db016ea1ceecc78e7e1331cb84bc972a620
João Moraes <joao.moraes@ivoiredesign.com.br>
2020-02-27 16:02:21 -0300

    Mostra o ultimo estado do HashtagBoard

--- src/components/HashtagBoard/index.js
+++ src/components/HashtagBoard/index.js
@@ -6,7 +6,7 @@ import PlayerGame from "../../objects/PlayerGame";
 const HashtagBoard = ({ history }) => {
 
     const [nextPlayer, setNextPlayer] = useState("x");
-    const [round, setRound] = useState(0);
+    const [lastRound, setLastRound] = useState(1);
     const [boardHistory, setBoardHistory] = useState([
         {
             round: 0,
@@ -21,24 +21,27 @@ const HashtagBoard = ({ history }) => {
                 {id: 8, content: ''},
                 {id: 9, content: ''},
             ]
+        },
+        {
+            round: 0,
+            state: [
+                {id: 1, content: ''},
+                {id: 2, content: ''},
+                {id: 3, content: ''},
+                {id: 4, content: ''},
+                {id: 5, content: 'x'},
+                {id: 6, content: ''},
+                {id: 7, content: ''},
+                {id: 8, content: ''},
+                {id: 9, content: ''},
+            ]
         }
-    ]);
-    const [players, setPlayers] = useState([
-        {id: 1, content: ''},
-        {id: 2, content: ''},
-        {id: 3, content: ''},
-        {id: 4, content: ''},
-        {id: 5, content: ''},
-        {id: 6, content: ''},
-        {id: 7, content: ''},
-        {id: 8, content: ''},
-        {id: 9, content: ''},
     ]);
 
     const handleClick = (id) => {
         setNextPlayer(old => old === "x" ? "o" : "x" );
         
-        setPlayers(old => old.map(player => player.id === id ? {id, content: nextPlayer} : player));
+        // setPlayers(old => old.map(player => player.id === id ? {id, content: nextPlayer} : player));
 
         history(nextPlayer);
     };
@@ -46,7 +49,7 @@ const HashtagBoard = ({ history }) => {
     return (
         <BoardGame>
             <ul className="hashtag-board">
-                {players.map(player => (
+                {boardHistory[lastRound].state.map(player => (
                     <li key={player.id} className="item" onClick={() => player.content === '' && handleClick(player.id)}>
                         <PlayerGame id={player.id} content={player.content} />
                     </li>
