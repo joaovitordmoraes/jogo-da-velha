b03fa6b864210836502c8b770634c8b0eefd7f1c
Joao <joaovitordmoraes@gmail.com>
2020-03-02 12:48:20 -0300

    Movendo os childrens para o HashtagBoardWrapper

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, {useState} from "react";
 import "./App.css";
 
-import CheckboxEvents from "./objects/CheckboxEvents";
 import AboutMenu from "./objects/AboutMenu";
 import HeaderGame from "./components/HeaderGame";
-import HashtagBoard from "./components/HashtagBoard";
 import HeaderInternal from "./components/HeaderInternal";
 import ProfileUser from "./components/ProfileUser";
-import PlayerEvents from "./components/PlayerEvents";
 import HashtagBoardWrapper from "./objects/HashtagBoardWrapper";
 
 
 const App = () => {
 
     const [activeAbout, setActiveAbout] = useState("");
-    const [history, setHistory] = useState(["Start"]);
 
     const handleClickAdd = () => {
         setActiveAbout("-active");
@@ -24,24 +20,11 @@ const App = () => {
         setActiveAbout("");
     }
 
-    const addEvents = (player) => {
-        setHistory(old => [...old, `Adicionou ${player.toUpperCase()}`]);
-    }
-
-    const changeHistory = key => {
-        setHistory(old => old.slice(0, key + 1));
-    }
-
     return (
         <main id="main" className="app">
             <HeaderGame onClick={handleClickAdd} />
 
-            <HashtagBoardWrapper>
-                <HashtagBoard history={addEvents} />
-                <CheckboxEvents id="show" value="show" content="Mostrar eventos" type="checkbox"  />
-                
-                <PlayerEvents history={history} onClick={changeHistory} />
-            </HashtagBoardWrapper>
+            <HashtagBoardWrapper />
 
             <AboutMenu className={activeAbout}>
                 <HeaderInternal onClick={handleClickRemove} />
--- src/objects/HashtagBoardWrapper/index.js
+++ src/objects/HashtagBoardWrapper/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import PlayerEvents from "../../components/PlayerEvents";
+import HashtagBoard from "../../components/HashtagBoard";
+import CheckboxEvents from "../CheckboxEvents";
 
 const HashtagBoardWrapper = ({ children }) => {
+    const [history, setHistory] = useState(["Start"]);
+
+    const addEvents = (player) => {
+        setHistory(old => [...old, `Adicionou ${player.toUpperCase()}`]);
+    }
+
+    const changeHistory = key => {
+        setHistory(old => old.slice(0, key + 1));
+    }
 
     return (
         <div className="hashtagboard-wrapper">
-            {children}
+            <HashtagBoard history={addEvents} />
+            <CheckboxEvents id="show" value="show" content="Mostrar eventos" type="checkbox"  />
+            
+            <PlayerEvents history={history} onClick={changeHistory} />
         </div>
     );
 }
