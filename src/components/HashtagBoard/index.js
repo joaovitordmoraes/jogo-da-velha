a33d26aaf46e83714569714638f184dad1db2cbc
Joao <joaovitordmoraes@gmail.com>
2020-03-06 18:02:42 -0300

    Feita a máquina do tempo para voltar as jogadas anteriores

--- src/components/HashtagBoard/index.js
+++ src/components/HashtagBoard/index.js
@@ -1,44 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import BoardGame from "../../objects/BoardGame";
 import PlayerGame from "../../objects/PlayerGame";
 
-const HashtagBoard = ({ history }) => {
 
-    const [nextPlayer, setNextPlayer] = useState("x");
-    const [lastRound, setLastRound] = useState(0);
-    const [boardHistory, setBoardHistory] = useState([
-        {
-            round: 0,
-            state: [
-                {id: 1, content: ''},
-                {id: 2, content: ''},
-                {id: 3, content: ''},
-                {id: 4, content: ''},
-                {id: 5, content: ''},
-                {id: 6, content: ''},
-                {id: 7, content: ''},
-                {id: 8, content: ''},
-                {id: 9, content: ''},
-            ]
-        }
-    ]);
-
-    const handleClick = (id) => {
-        setNextPlayer(old => old === "x" ? "o" : "x" );
-        setBoardHistory(old => [...old, {
-            round: lastRound + 1,
-            state: old[lastRound].state.map(player => player.id === id ? {id, content: nextPlayer} : player)
-        }]);
-        setLastRound(old => old + 1);
-        history(nextPlayer);
-    };
+const HashtagBoard = ({ boardHistory, onClick, lastRound }) => {
 
     return (
         <BoardGame>
             <ul className="hashtag-board">
                 {boardHistory[lastRound].state.map(player => (
-                    <li key={player.id} className="item" onClick={() => player.content === '' && handleClick(player.id)}>
+                    <li key={player.id} className="item" onClick={() => player.content === '' && onClick(player.id)}>
                         <PlayerGame id={player.id} content={player.content} />
                     </li>
                 ))}
--- src/objects/HashtagBoardWrapper/index.js
+++ src/objects/HashtagBoardWrapper/index.js
@@ -3,8 +3,11 @@ import PlayerEvents from "../../components/PlayerEvents";
 import HashtagBoard from "../../components/HashtagBoard";
 import CheckboxEvents from "../CheckboxEvents";
 
-const HashtagBoardWrapper = ({ children }) => {
+const HashtagBoardWrapper = () => {
     const [history, setHistory] = useState(["Start"]);
+    const [active, setActive] = useState(false);
+    const [nextPlayer, setNextPlayer] = useState("x");
+    const [lastRound, setLastRound] = useState(0);
 
     const addEvents = (player) => {
         setHistory(old => [...old, `Adicionou ${player.toUpperCase()}`]);
@@ -12,11 +15,40 @@ const HashtagBoardWrapper = ({ children }) => {
 
     const changeHistory = key => {
         setHistory(old => old.slice(0, key + 1));
+        setLastRound(key);
+        setBoardHistory(old => old.slice(0, key + 1));
     }
 
+    const [boardHistory, setBoardHistory] = useState([
+        {
+            round: 0,
+            state: [
+                {id: 1, content: ''},
+                {id: 2, content: ''},
+                {id: 3, content: ''},
+                {id: 4, content: ''},
+                {id: 5, content: ''},
+                {id: 6, content: ''},
+                {id: 7, content: ''},
+                {id: 8, content: ''},
+                {id: 9, content: ''},
+            ]
+        }
+    ]);
+
+    const handleClick = (id) => {
+        setNextPlayer(old => old === "x" ? "o" : "x" );
+        setBoardHistory(old => [...old, {
+            round: lastRound + 1,
+            state: old[lastRound].state.map(player => player.id === id ? {id, content: nextPlayer} : player)
+        }]);
+        setLastRound(old => old + 1);
+        addEvents(nextPlayer);
+    };
+
     return (
-        <div className="hashtagboard-wrapper">
-            <HashtagBoard history={addEvents} />
+        <div className={`hashtagboard-wrapper ${active && "-active"}`}>
+            <HashtagBoard boardHistory={boardHistory} onClick={handleClick} lastRound={lastRound} />
             <CheckboxEvents id="show" value="show" content="Mostrar eventos" type="checkbox"  />
             
             <PlayerEvents history={history} onClick={changeHistory} />
